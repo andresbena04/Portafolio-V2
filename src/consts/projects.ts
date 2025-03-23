a78f07d64d5adb71c2fe2f6961a391d32ad33563
Andres Benavides <andresbena3515@gmail.com>
2025-03-23 11:06:49 -0500

    refactor: update project assets and paths, remove unused images

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -1,54 +1,30 @@
-/* export const projects = [
-    {
-        title: "Sistema de Soporte de Tickets",
-        description: "Aplicación para gestionar tickets de soporte, donde los usuarios pueden enviar problemas y recibir soluciones de los agentes de soporte.",
-        image: "/assets/projects/support-ticket-system.jpg",
-        link: "https://github.com/andresbena04/support-ticket-system"
-    },
-    {
-        title: "Factus API Integration",
-        description: "Proyecto de integración con la API de Factus para la gestión de facturas, productos y clientes.",
-        image: "/assets/projects/factus-api-integration.jpg",
-        link: "https://github.com/andresbena04/factus-api-integration"
-    },
-    {
-        title: "Sistema POS con NestJS",
-        description: "Sistema POS para la gestión de ventas, usuarios, productos e inventarios en diversos negocios, utilizando NestJS y Prisma.",
-        image: "/assets/projects/pos-system-nestjs.jpg",
-        link: "https://github.com/andresbena04/pos-system-nestjs"
-    }
-
-]; */
 export const projects = [
     {
       title: "Sistema POS con NestJS",
       description: "Sistema POS para la gestión de ventas, usuarios, productos e inventarios en diversos negocios, utilizando NestJS y Prisma.",
-      image: "https://www.comparapps.com/wp-content/uploads/2022/03/sistemas-pos.png",
       link: "https://github.com/andresbena04/pos-system-nestjs"
     },
     {
       title: "Factus API Integration",
       description: "Proyecto de integración con la API de Factus para la gestión de facturas electrónicas.",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3qyTW8s6UfaD4vVMLkLR7kgq7r3v_FOkjPg&s",
       link: "https://github.com/andresbena04/factus-api-integration"
     },
     {
       title: "Sistema de Soporte de Tickets",
       description: "Aplicación para gestionar tickets de soporte, donde los usuarios pueden enviar problemas y recibir soluciones de los agentes de soporte.",
-      image: "https://lone.design/wp-content/uploads/2022/06/TicketSupportSystem.png",
       link: "https://github.com/andresbena04/support-ticket-system"
     },
     {
       title: "CineApp",
       description: "Aplicación en Angular para visualizar la información de las películas en cartelera y los próximos estrenos.",
-      image: "https://github.com/andresbena04/cine-angular/raw/main/src/assets/cineapp-preview.png",
-      link: "https://github.com/andresbena04/cine-angular"
+      link: "https://github.com/andresbena04/cine-angular",
+      demo: 'https://cine-angular.vercel.app'
     },
     {
       title: "Cabaña Ladera Verde",
       description: "Landing page para la promoción de la cabaña Ladera Verde, destacando sus servicios, galería e información de contacto.",
-      image: "https://github.com/tu-usuario/landing-cabana/raw/main/assets/preview.png",
-      link: "https://github.com/andresbena04/ladera-verde-landing.git"
+      link: "https://github.com/andresbena04/ladera-verde-landing.git",
+      demo: "https://ladera-verde-landing.vercel.app"
     }
     
   ]
--- src/consts/techIcons.ts
+++ src/consts/techIcons.ts
@@ -6,14 +6,16 @@ import nodejs from '../assets/tech/nodedotjs.svg';
 import express from '../assets/tech/express.svg';
 import mysql from '../assets/tech/mysql.svg';
 import postgresql from '../assets/tech/postgresql.svg';
+import astro from '../assets/tech/astro.svg';
 
 export const techIcons =  [
-    { name: 'TypeScript', icon: typeScriptIcon, alt: 'TypeScript' },
-    { name: 'JavaScript', icon: javaScriptIcon, alt: 'JavaScript' },
     { name: 'Angular', icon: angular, alt: 'Angular' },
-    { name: 'NestJS', icon: nestjs, alt: 'NestJS' },
+    { name: 'Astro', icon: astro, alt: 'Astro' },
+    { name: 'JavaScript', icon: javaScriptIcon, alt: 'JavaScript' },
     { name: 'NodeJS', icon: nodejs, alt: 'NodeJS' },
+    { name: 'NestJS', icon: nestjs, alt: 'NestJS' },
     { name: 'Express', icon: express, alt: 'Express' },
+    { name: 'TypeScript', icon: typeScriptIcon, alt: 'TypeScript' },
     { name: 'MySQL', icon: mysql, alt: 'MySQL' },
     { name: 'PostgreSQL', icon: postgresql, alt: 'PostgreSQL' },
 ]
