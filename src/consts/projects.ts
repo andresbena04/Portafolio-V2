ca15b065e1917390634b9045d14a6645b82fffd8
Andres Benavides <andresbena3515@gmail.com>
2025-03-26 21:22:18 -0500

    feat: enhance project section with technology details

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -1,31 +1,34 @@
 export const projects = [
-    {
-      title: "Sistema POS con NestJS",
-      description: "Sistema POS para la gestión de ventas, usuarios, productos e inventarios en diversos negocios, utilizando NestJS y Prisma.",
-      link: "https://github.com/andresbena04/pos-system-nestjs"
-    },
-    {
-      title: "Factus API Integration",
-      description: "Proyecto de integración con la API de Factus para la gestión de facturas electrónicas.",
-      link: "https://github.com/andresbena04/factus-api-integration"
-    },
-    {
-      title: "Sistema de Soporte de Tickets",
-      description: "Aplicación para gestionar tickets de soporte, donde los usuarios pueden enviar problemas y recibir soluciones de los agentes de soporte.",
-      link: "https://github.com/andresbena04/support-ticket-system"
-    },
-    {
-      title: "CineApp",
-      description: "Aplicación en Angular para visualizar la información de las películas en cartelera y los próximos estrenos.",
-      link: "https://github.com/andresbena04/cine-angular",
-      demo: 'https://cine-angular.vercel.app'
-    },
-    {
-      title: "Cabaña Ladera Verde",
-      description: "Landing page para la promoción de la cabaña Ladera Verde, destacando sus servicios, galería e información de contacto.",
-      link: "https://github.com/andresbena04/ladera-verde-landing.git",
-      demo: "https://ladera-verde-landing.vercel.app"
-    }
-    
-  ]
-  
+  {
+    title: "Sistema POS con NestJS",
+    description: "Sistema POS para la gestión de ventas, usuarios, productos e inventarios en diversos negocios, utilizando NestJS y Prisma.",
+    technologies: ["NestJS", "Prisma", "PostgreSQL"],
+    link: "https://github.com/andresbena04/pos-system-nestjs"
+  },
+  {
+    title: "Factus API Integration",
+    description: "Proyecto de integración con la API de Factus para la gestión de facturas electrónicas.",
+    technologies: ["Angular"],
+    link: "https://github.com/andresbena04/factus-api-integration"
+  },
+  {
+    title: "Sistema de Soporte de Tickets",
+    description: "Aplicación para gestionar tickets de soporte, donde los usuarios pueden enviar problemas y recibir soluciones de los agentes de soporte.",
+    technologies: ["Angular", "NestJS", "PostgreSQL", "TypeScript"],
+    link: "https://github.com/andresbena04/support-ticket-system"
+  },
+  {
+    title: "CineApp",
+    description: "Aplicación en Angular para visualizar la información de las películas en cartelera y los próximos estrenos.",
+    technologies: ["Angular"],
+    link: "https://github.com/andresbena04/cine-angular",
+    demo: "https://cine-angular.vercel.app"
+  },
+  {
+    title: "Cabaña Ladera Verde",
+    description: "Landing page para la promoción de la cabaña Ladera Verde, destacando sus servicios, galería e información de contacto.",
+    technologies: ["Astro"],
+    link: "https://github.com/andresbena04/ladera-verde-landing.git",
+    demo: "https://ladera-verde-landing.vercel.app"
+  }
+];
