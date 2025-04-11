102597682522a71a2319fcd9abe02b727b5bafe3
Andres Benavides <andresbena3515@gmail.com>
2025-04-10 21:29:18 -0500

    feat: Add TechIconsScroller component and integrate it into Projects section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,20 @@
 import animations from '@midudev/tailwind-animations'
 
 export default {
-    content: ["./src/**/*.{astro,js,ts,jsx,tsx,html}"],
-    darkMode: "class",
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx,html}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }
     },
-    plugins: [animations],
-  };
-  
+  },
+  plugins: [animations],
+};
